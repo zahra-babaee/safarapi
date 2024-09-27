8ae64fcfbd0cf2608a1d4c21d056cc693b1d9c2a
Zahra <Zahra77babaee@gmail.com>
2024-09-27 20:40:03 +0330

    Initial commit

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\v1\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// مسیرهای عمومی (نیازی به احراز هویت JWT ندارند)
 Route::group([
     'middleware' => 'api',
     'prefix' => 'v1'
@@ -24,26 +23,4 @@ Route::group([
     Route::post('login_password', [\App\Http\Controllers\v1\AuthController::class, 'loginWithPass']);
 
 });
-
-
-//    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
-//    Route::post('/profile', [AuthController::class, 'profile'])->middleware('auth:api');
-
-
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
-
-//Route::group(['prefix' => 'v1'], function ($router) {
-//    $router->get('users', [\App\Http\Controllers\v1\user\UserController::class, 'index']);
-//    $router->get('upload', [\App\Http\Controllers\v1\user\UploadController::class, 'index']);
-//    $router->post('upload', [\App\Http\Controllers\v1\user\UploadController::class, 'store']);
-
-//    $router->post('register', [\App\Http\Controllers\v1\AuthController::class, 'register']);
-//    $router->post('verify-otp', [\App\Http\Controllers\v1\AuthController::class, 'verifyOTP']);
-//    $router->post('resend-otp', [\App\Http\Controllers\v1\AuthController::class, 'resendOTP']);
-
-//    $router->post('login_password',[\App\Http\Controllers\v1\AuthController::class, 'loginWithPass']);
-//
-
 
