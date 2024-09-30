f49b0ccac656a85414b7284aac9a13ca61670ca0
Zahra <Zahra77babaee@gmail.com>
2024-09-30 20:51:32 +0330

    ttl_20

--- app/Http/Controllers/v1/AuthController.php
+++ app/Http/Controllers/v1/AuthController.php
@@ -69,10 +69,11 @@ class AuthController extends Controller
             // اگر از ارسال آخرین OTP کمتر از 120 ثانیه گذشته باشد
             if ($timeSinceLastOtp < 120) {
                 // محاسبه زمان باقی‌مانده از 120 ثانیه و اطمینان از اینکه عدد صحیح و غیر منفی باشد
+                $remainingSeconds = max(0, 120 - $timeSinceLastOtp);
 
                 return response()->json([
                     'message' => 'لطفاً قبل از درخواست جدید ۱۲۰ ثانیه صبر کنید.',
-                    'otp_ttl' => $timeSinceLastOtp, // مقدار TTL به ثانیه
+                    'otp_ttl' => $remainingSeconds, // مقدار TTL به ثانیه
                     'has_account' => false
                 ], 429);
             }
