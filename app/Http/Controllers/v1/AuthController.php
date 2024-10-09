04809779bc2468edc262f340c9dfbe7e621b488b
Zahra <Zahra77babaee@gmail.com>
2024-10-09 15:56:32 +0330

    BaseDto response-authcontroller

--- app/Http/Controllers/v1/AuthController.php
+++ app/Http/Controllers/v1/AuthController.php
@@ -69,9 +69,20 @@ class AuthController extends Controller
         // اعتبارسنجی شماره تلفن
         $validator = Validator::make($request->all(), [
             'phone' => 'required|regex:/^09[0-9]{9}$/|digits:11',
+        ], [
+            'phone.required' => 'شماره تلفن نباید خالی باشد.',
+            'phone.regex' => 'شماره تلفن باید با 09 شروع شود و 11 رقم باشد.',
+            'phone.digits' => 'شماره تلفن باید دقیقاً 11 رقم باشد.'
         ]);
+
         if ($validator->fails()) {
-            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR, $validator->errors()), 400);
+            $errors = $validator->errors()->toArray();
+
+            return response()->json(new BaseDto(
+                BaseDtoStatusEnum::ERROR,
+                'خطاهای اعتبارسنجی رخ داده است.',
+                $errors
+            ), 400);
         }
 
         $otpValidityDuration = 120; // اعتبار OTP به ثانیه
@@ -85,7 +96,6 @@ class AuthController extends Controller
             if ($timeSinceLastOtp < $otpValidityDuration) {
                 $remainingSeconds = $otpValidityDuration - $timeSinceLastOtp;
 
-                // کاربر تلاش می‌کند دوباره در زمان کمتر از 2 دقیقه درخواست OTP بدهد
                 return new BaseDto(
                     BaseDtoStatusEnum::ERROR,
                     "تا {$remainingSeconds} ثانیه دیگر نمی‌توانید درخواست جدید بدهید.",
@@ -94,12 +104,11 @@ class AuthController extends Controller
                         'otp_ttl' => $remainingSeconds,
                         'has_account' => (bool)$user,
                         'has_password' => $user ? $user->has_password : false,
-                    ]
+                    ],
                 );
             }
         }
 
-        // اگر OTP قبلی منقضی شده یا وجود ندارد، یک OTP جدید تولید کنید
         $otp = rand(1000, 9999);
         Otp::query()->create([
             'phone' => $request->phone,
@@ -171,12 +180,28 @@ class AuthController extends Controller
      */
     public function verifyOtp(Request $request)
     {
+//        $validator = Validator::make($request->all(), [
+//            'phone' => 'required|regex:/^09[0-9]{9}$/|digits:11',
+//            'otp' => 'required|digits:4',
+//        ]);
         $validator = Validator::make($request->all(), [
             'phone' => 'required|regex:/^09[0-9]{9}$/|digits:11',
             'otp' => 'required|digits:4',
+            ], [
+            'phone.required' => 'شماره تلفن نباید خالی باشد.',
+            'phone.regex' => 'شماره تلفن باید با 09 شروع شود و 11 رقم باشد.',
+            'phone.digits' => 'شماره تلفن باید دقیقاً 11 رقم باشد.',
+            'otp.required' => 'کدیکبار مصرف را وارد کنید.',
+            'otp.digits' => 'کدیکبار مصرف باید 4 رقم باشد'
         ]);
-        if($validator->fails()){
-            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR, $validator->errors()), 400);
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+
+            return response()->json(new BaseDto(
+                BaseDtoStatusEnum::ERROR,
+                'خطاهای اعتبارسنجی رخ داده است.',
+                $errors
+            ), 400);
         }
 
         $otpRecord = Otp::query()
@@ -206,53 +231,56 @@ class AuthController extends Controller
             try {
                 $token = JWTAuth::fromUser($user);
             } catch (JWTException $e) {
-                return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR, data:
-                [
-                    'message' => 'مشکلی در ایجاد توکن به وجود آمده است.',
-                    'status' => '500',
-                ]));
+                return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR,
+                    'مشکلی در ایجاد توکن به وجود آمده است.', ),500);
             }
             // پاسخ با توکن JWT
-            return response()->json(new BaseDto(BaseDtoStatusEnum::OK, data: [
+            return response()->json(new BaseDto(BaseDtoStatusEnum::OK,
+                'ورود موفقیت آمیز',
+                data: [
                     'token' => $token,
                     'user Data' => $user,
-                ]));
+                ]),200);
         } else {
-            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR, data: [
-                'message' => 'کد OTP نامعتبر است یا منقضی شده است.',
-//                'status' => 422,
-            ]));
+            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR,
+                'کد OTP نامعتبر است یا منقضی شده است.'),422);
         }
     }
 
     public function loginWithPass(Request $request)
     {
-        // اعتبارسنجی ورودی‌ها
         $validator = Validator::make($request->all(), [
             'phone' => 'required|regex:/^09[0-9]{9}$/|digits:11',
-            'password' => 'required|string',
+            'password' => 'required',
+        ], [
+            'phone.required' => 'شماره تلفن نباید خالی باشد.',
+            'phone.regex' => 'شماره تلفن باید با 09 شروع شود و 11 رقم باشد.',
+            'phone.digits' => 'شماره تلفن باید دقیقاً 11 رقم باشد.',
+            'password.required' => 'رمزعبور نباید خالی باشد',
         ]);
         if ($validator->fails()) {
-            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR, $validator->errors()), 400);
+            $errors = $validator->errors()->toArray();
+
+            return response()->json(new BaseDto(
+                BaseDtoStatusEnum::ERROR,
+                'خطاهای اعتبارسنجی رخ داده است.',
+                $errors
+            ), 400);
         }
         $credentials = $request->only('phone', 'password');
         try {
             if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR, data:
-                    [
-                        'status'=>400,
-                        'error' => 'نام کاربری یا رمز عبور نادرست است'
-                    ]));
+                return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR,
+                    'نام کاربری یا رمز عبور نادرست است',),400);
             }
         } catch (JWTException $e) {
-            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR, data:
-                [
-                    'message' => 'امکان ایجاد توکن وجود ندارد. لطفاً دوباره تلاش کنید.'
-                ]),500);
+            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR,
+                'امکان ایجاد توکن وجود ندارد. لطفاً دوباره تلاش کنید.'),500);
         }
         $user = auth()->user();
 
-        return response()->json(data: new BaseDto(BaseDtoStatusEnum::OK, data: [
+        return response()->json(data: new BaseDto(BaseDtoStatusEnum::OK,'ورود موفقیت آمیز',
+            data: [
             'token' => $token,
             'user' => $user,
         ]));
@@ -262,27 +290,34 @@ class AuthController extends Controller
         {
             JWTAuth::invalidate(JWTAuth::getToken());
 
-            return response()->json(new BaseDto(BaseDtoStatusEnum::OK, data:
-                ['message' => 'Successfully logged out']));
+            return response()->json(new BaseDto(BaseDtoStatusEnum::OK,'Successfully logged out'),200);
         }
     }
     public function forgetPassword(Request $request)
     {
-        // اعتبارسنجی ورودی
         $validator = Validator::make($request->all(), [
             'phone' => 'required|regex:/^09[0-9]{9}$/|digits:11',
+        ], [
+            'phone.required' => 'شماره تلفن نباید خالی باشد.',
+            'phone.regex' => 'شماره تلفن باید با 09 شروع شود و 11 رقم باشد.',
+            'phone.digits' => 'شماره تلفن باید دقیقاً 11 رقم باشد.'
         ]);
+
         if ($validator->fails()) {
-            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR, $validator->errors()), 400);
+            $errors = $validator->errors()->toArray();
+
+            return response()->json(new BaseDto(
+                BaseDtoStatusEnum::ERROR,
+                'خطاهای اعتبارسنجی رخ داده است.',
+                $errors
+            ), 400);
         }
 
         // بررسی وجود کاربر
         $user = User::query()->where('phone', $request->phone)->first();
 
         if (!$user) {
-            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR, data: [
-                'message' => 'کاربر با این شماره تلفن وجود ندارد.']
-            ),404);
+            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR,'کاربری با این شماره تلفن وجود ندارد.'),404);
         }
         else{
             $lastOtp = Otp::query()->where('phone', $request->phone)->orderBy('created_at', 'desc')->first();
@@ -291,9 +326,8 @@ class AuthController extends Controller
                 $remainingTime = Otp::remainingTime($request->phone, 2);
 
                 if ($remainingTime > 0) {
-                    return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR , data:[
-//                        'status'=>429,
-                        'message' => 'لطفاً قبل از درخواست جدید دو دقیقه صبر کنید.',
+                    return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR,'لطفاً قبل از درخواست جدید دو دقیقه صبر کنید.',
+                        data:[
                         'otp_ttl' => $remainingTime,
                     ]),429);
                 }
@@ -307,27 +341,50 @@ class AuthController extends Controller
             ]);
 
             // ارسال OTP به شماره تلفن کاربر
-            $this->sendOtp($request->phone, $otp); // متدی برای ارسال OTP
+            $this->sendOtp($request->phone, $otp);
 
-            return response()->json(new BaseDto(BaseDtoStatusEnum::OK, data:
+            return response()->json(new BaseDto(BaseDtoStatusEnum::OK,'کد یکبار مصرف برای بازنشانی رمز عبور ارسال شد.',
+                data:
                 [
-                'message' => 'کد یکبار مصرف برای بازنشانی رمز عبور ارسال شد.',
                 'otp_ttl' => $remainingTime,
-                 ]));
+                 ]),200);
         }
     }
     public function resetPassword(Request $request)
     {
-        // اعتبارسنجی ورودی
         $validator = Validator::make($request->all(), [
             'phone' => 'required|regex:/^09[0-9]{9}$/|digits:11',
             'otp' => 'required|digits:4',
-            'password' => 'required|min:6|confirmed', // اطمینان از تایید رمز عبور
+            'password' => [
+                'required',
+                'min:8',
+                'confirmed', 'regex:/[a-z]/', 'regex:/[A-Z]/', 'regex:/[0-9]/', 'regex:/[@$!%*#?&]/'
+            ]
+        ], [
+            'phone.required' => 'شماره تلفن نباید خالی باشد.',
+            'phone.regex' => 'شماره تلفن باید با 09 شروع شود و 11 رقم باشد.',
+            'phone.digits' => 'شماره تلفن باید دقیقاً 11 رقم باشد.',
+            'otp.required' => 'کد یکبار مصرف را وارد کنید.',
+            'otp.digits' => 'کد یکبار مصرف باید 4 رقم باشد.',
+            'password.required' => 'رمز عبور نباید خالی باشد.',
+            'password.min' => 'رمز عبور باید حداقل 8 کاراکتر باشد.',
+            'password.confirmed' => 'رمز عبور و تأیید آن مطابقت ندارند.',
+            'password.regex' => [
+                'regex:/[a-z]/' => 'رمز عبور باید حداقل یک حرف کوچک داشته باشد.',
+                'regex:/[A-Z]/' => 'رمز عبور باید حداقل یک حرف بزرگ داشته باشد.',
+                'regex:/[0-9]/' => 'رمز عبور باید حداقل یک عدد داشته باشد.',
+                'regex:/[@$!%*#?&]/' => 'رمز عبور باید حداقل یک کاراکتر خاص (مثل @$!%*#?&) داشته باشد.',
+            ]
         ]);
         if ($validator->fails()) {
-            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR, $validator->errors()), 400);
-        }
+            $errors = $validator->errors()->toArray();
 
+            return response()->json(new BaseDto(
+                BaseDtoStatusEnum::ERROR,
+                'خطاهای اعتبارسنجی رخ داده است.',
+                $errors
+            ), 400);
+        }
         // جستجوی کد OTP در دیتابیس
         $otpRecord = Otp::query()
             ->where('phone', $request->phone)
@@ -336,10 +393,7 @@ class AuthController extends Controller
             ->first();
 
         if (!$otpRecord) {
-            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR, data:
-            [
-                'message' => 'کد OTP نامعتبر است یا منقضی شده است.'
-            ]),422);
+            return response()->json(new BaseDto(BaseDtoStatusEnum::ERROR,'کد OTP نامعتبر است یا منقضی شده است.'), 422);
         }
 
         // بروزرسانی رمز عبور کاربر
@@ -349,18 +403,18 @@ class AuthController extends Controller
         // حذف رکورد OTP بعد از استفاده
         $otpRecord->delete();
 
-        return response()->json(new BaseDto(BaseDtoStatusEnum::OK, data:
-        ['message' => 'رمز عبور با موفقیت بازنشانی شد.']));
+        return response()->json(new BaseDto(BaseDtoStatusEnum::OK,'رمز عبور با موفقیت بازنشانی شد.'),
+            200);
     }
 
     private function sendOtp($phone, $otp)
     {
         // ارسال OTP به شماره تلفن
         $curl = curl_init();
-        $apikey = '493036347A343565484D3767455769504867546F636A7A30664D6C36316F724E38654E2B42324A2F4166633D'; // کلید API شما
-        $message = urlencode("کد یکبار مصرف برای ورود شما: $otp");
+        $apikey = '493036347A343565484D3767455769504867546F636A7A30664D6C36316F724E38654E2B42324A2F4166633D';
+        $message = urlencode("Your verification is: $otp");
 //        $phone = $request->phone;
-        $sender = '100090003'; // شماره فرستنده شما
+        $sender = '100090003';
         $url = "https://api.kavenegar.com/v1/$apikey/sms/send.json?receptor=$phone&sender=$sender&message=$message";
 
         curl_setopt_array($curl, array(
