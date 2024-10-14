c7f071c8efa614786321656d8740ea9715656f68
Zahra <Zahra77babaee@gmail.com>
2024-10-14 12:03:54 +0330

    APP_URL=Change

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,12 +37,18 @@ return [
             'throw' => false,
         ],
 
+//        'public' => [
+//            'driver' => 'local',
+//            'root' => storage_path('app/public'),
+//            'url' => env('APP_URL').'/storage',
+//            'visibility' => 'public',
+//            'throw' => false,
+//        ],
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
-            'throw' => false,
         ],
 
         's3' => [
